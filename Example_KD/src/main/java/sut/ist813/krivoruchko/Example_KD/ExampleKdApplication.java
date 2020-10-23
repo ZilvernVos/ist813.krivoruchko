f8b2dd96d5c843168fc4fced5211046e300482b8
ZilvernVos <71167890+ZilvernVos@users.noreply.github.com>
2020-10-23 19:53:46 +0300

    adding 2 files
    adding 2 files

---
+++ Example_KD/src/main/java/sut/ist813/krivoruchko/Example_KD/ExampleKdApplication.java
@@ -0,0 +1,13 @@
+package sut.ist813.krivoruchko.Example_KD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleKdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleKdApplication.class, args);
+	}
+
+}
---
+++ Example_KD/src/test/java/sut/ist813/krivoruchko/Example_KD/ExampleKdApplicationTests.java
@@ -0,0 +1,13 @@
+package sut.ist813.krivoruchko.Example_KD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleKdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
